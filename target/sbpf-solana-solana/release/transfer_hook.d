7c5446219afe33a8e558f94893165e09b057bb1c
AshwiniRev <ashwini@revinfotech.com>
2025-10-08 10:14:42 +0530

    init

--- target/sbpf-solana-solana/release/transfer_hook.d
+++ target/sbpf-solana-solana/release/transfer_hook.d
@@ -1,1 +1,1 @@
-/Users/ashwiniupadhyay/hello-world-transfer-hook-token2022/target/sbpf-solana-solana/release/transfer_hook.so: /Users/ashwiniupadhyay/hello-world-transfer-hook-token2022/programs/transfer-hook/src/lib.rs
+/Users/ashwiniupadhyay/Solana-Wcrotch/target/sbpf-solana-solana/release/transfer_hook.so: /Users/ashwiniupadhyay/Solana-Wcrotch/programs/transfer-hook/src/lib.rs
